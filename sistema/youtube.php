0d62c7f853860bc3cab4b796bd3fbe07496ca8f3
andreAguia <alataguia@gmail.com>
2017-11-27 22:16:29 -0200

    classe MenuVideo

--- sistema/youtube.php
+++ sistema/youtube.php
@@ -22,28 +22,61 @@ $page->iniciaPagina();
 # Cabeçalho
 cabecalho();
 
-$grid = new Grid();
-$grid->abreColuna(12);
+$grid1 = new Grid();
+$grid1->abreColuna(12);
+
+# Título
+botaoVoltar("menuInicial.php");        
+titulo('Filmes');
+br();
+
+$grid2 = new Grid();
+$grid2->abreColuna(2);
+
+    $menu = new Menu();
+    $menu->add_item('titulo',"Categoria","#");
+    $menu->add_item('link','Pocoyo','?fase=pocoyo');
+    $menu->add_item('link','Bita','?fase=bita');
+    $menu->show();
+    br();    
+
+ $grid2->fechaColuna();
+ $grid2->abreColuna(10);
 
 switch ($fase){	
     # Exibe o Menu de Vídeos
-    case "menu" :
-        botaoVoltar("menuInicial.php");        
-        titulo('Filmes');
-        br();
-        
-        $grid = new Grid();
-        
+    case "pocoyo" :
         $video = new MenuVideo();
         $video->add_item("ytVideo","5fNN4uNtLp0","Coleção nº1");
         $video->add_item("ytVideo","SQTk4fgjpHI","Coleção nº2");
         $video->add_item("ytVideo","uPDB5I9wfp8","Coleção nº3");
         $video->show();
-        
-        $grid->fechaGrid();
+        break;
+    
+     case "bita" :
+        $video = new MenuVideo();
+        $video->add_item("ytVideo","0GIgk4yuHOQ","Dinossauro");
+        $video->add_item("ytVideo","Oa5BaUBXpho","Festa na Lagoa");
+        $video->add_item("ytVideo","5TVsXxsFJps","De Estimacao");
+        $video->add_item("ytVideo","j7A9ANT2aVQ","Insetos");
+        $video->add_item("ytVideo","cjONzZPJONc","Fazendinha");
+        $video->add_item("ytVideo","iY91JoMWQoM","No Fundo do Mar");
+        $video->add_item("ytVideo","2ZB6dQ-Fjho","Como e Verde na Floresta");
+        $video->add_item("ytVideo","rUeaT5eqCyg","Nem Tudo que Sobra e Lixo");
+        $video->add_item("ytVideo","hgFfC4cCnec","Voa Passarinho");
+        $video->add_item("ytVideo","eLtzvypcurE","A Diferença é o que nos une");
+        $video->add_item("ytVideo","eulFsmcNHos","Natal do Bita");
+        $video->add_item("ytVideo","4hmTy4mTCtc","A Boneca e o Boneco");
+        $video->add_item("ytVideo","cnzgHAIRqmc","Xic Xic Xic");
+        $video->add_item("ytVideo","31iBxkTTAfc","Bom Banho");
+        $video->show();
         break;
 }
-$grid->fechaColuna(); 
-$grid->fechaGrid();
+
+$grid2->fechaColuna();
+$grid2->fechaGrid();
+ 
+$grid1->fechaColuna(); 
+$grid1->fechaGrid();
 
 $page->terminaPagina();
